31d352a3d17657608a243057a1230910b6c63fd7
Mau Ruanova <mruanova@gmail.com>
2024-10-11 19:45:11 -0500

    MusicKit.getInstance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+declare var MusicKit: any; // Or declare specific function names if known
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apple-music';
+  title = 'Apple Music';
+  async onClick(event: MouseEvent) {
+    const music = MusicKit.getInstance();
+    if (music) {
+      // Music previews can be played without authorization:
+      await music.play();
+      // To play the full length of a song, check authorization before calling play():
+      await music.authorize();
+      await music.play();
+      // You should check authorization before accessing user's iCloud Music Library:
+      await music.authorize();
+      //const { data: result } = await music.api.music('v1/me/library/albums');
+      const result = await music.api.search(
+        `/v1/catalog/us/search`,
+        { term: 'SEARCH_TERM', types: 'albums' }
+      );
+      // User's iCloud Music Library Albums
+      console.log('result', result);
+    } else {
+      console.log(event);
+    }
+  };
 }
